a9b4dfea4caa4e5cee00c73cf47c8f47208f9730
SaM-92 <sam.misaqian@gmail.com>
2023-12-20 15:37:35 +0000

    add key vault values for workspace

---
+++ src/train-model-mlflow.py
@@ -0,0 +1,103 @@
+# import libraries
+import mlflow
+import argparse
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MaxAbsScaler
+from xgboost import XGBRegressor
+
+from sklearn.metrics import mean_squared_error,r2_score
+def main(args):
+    # read data
+    df = get_data(args.training_data)
+
+    # split data
+    X_train, X_test, y_train, y_test = split_data(df)
+
+    # train model
+    model = train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+
+    # evaluate model
+    eval_model(model, X_test, y_test)
+
+# function that reads the data
+def get_data(path):
+    print("Reading data from dataset")
+    df = pd.read_csv(args)
+    df= df.iloc[:768,:10]
+    
+    return df
+
+# function that splits the data
+def split_data(df):
+    print("Splitting data...")
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness',
+    'SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+    return X_train, X_test, y_train, y_test
+
+# function that trains the model
+def train_model(X_train, y_train):
+    print("Training model...")
+    
+    # Create a pipeline
+    pipeline = Pipeline([
+        ('scaler', MaxAbsScaler()),  # Normalize data
+        ('model', XGBRegressor())  # XGBoost model
+    ])
+    
+    # Train the model
+    model = pipeline.fit(X_train, y_train)
+    
+    return model
+
+# function that evaluates the model
+def eval_model(model, X_test, y_test):
+    # calculate predictions
+    y_pred = model.predict(X_test)
+    
+    # calculate RMSE
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    print('Root Mean Squared Error:', rmse)
+    mlflow.log_metric("RMSE", rmse)
+
+    # calculate R-squared
+    r2 = r2_score(y_test, y_pred)
+    print('R-squared: ', r2)
+    mlflow.log_metric("R-squared", r2)
+
+def parse_args():
+    # setup arg parser
+    parser = argparse.ArgumentParser()
+
+    # add arguments
+    parser.add_argument("--training_data", dest='training_data',
+                        type=str)
+    parser.add_argument("--reg_rate", dest='reg_rate',
+                        type=float, default=0.01)
+
+    # parse args
+    args = parser.parse_args()
+
+    # return args
+    return args
+
+# run script
+if __name__ == "__main__":
+    # add space in logs
+    print("\n\n")
+    print("*" * 60)
+
+    # parse args
+    args = parse_args()
+
+    # run main function
+    main(args)
+
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
